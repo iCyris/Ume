862f6eb64474e7f424d7a4e784fb35d9e51d22ae
iCyris <cyris1015@gmail.com>
2019-03-23 15:19:11 +0800

    add home link to sidebar (sp)

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -7,6 +7,9 @@
             </saber-link>
         </div>
         <div class="site-nav">
+            <div class="nav-item" v-if="checkWidth()">
+                <saber-link :to="spHome.link">{{ spHome.title }}</saber-link>
+            </div>
             <div class="nav-item" v-for="(item, index) in nav" :key="index">
                 <saber-link :to="item.link">{{ item.title }}</saber-link>
             </div>
@@ -28,8 +31,26 @@
                 title: themeConfig.title,
                 subTitle: themeConfig.subTitle,
                 nav: themeConfig.sideBar,
-                copyRight: themeConfig.copyRight
+                spHome: themeConfig.spHome,
+                screenWidth: typeof document === 'object' ? document.documentElement.clientWidth : ''
             }
+        },
+
+        mounted: function () {
+            window.addEventListener('resize', this.handleResize)
+        },
+        beforeDestroy: function () {
+            window.removeEventListener('resize', this.handleResize)
+        },
+
+        methods: {
+            handleResize (event) {
+                this.screenWidth = document.documentElement.clientWidth
+            },
+            checkWidth () {
+                return (this.screenWidth !== '' && this.screenWidth < 760)
+            }
+
         }
     }
 </script>
