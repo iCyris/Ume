59f800e26e3c732bdf9b9d4de5a08388e461e222
iCyris <cyris1015@gmail.com>
2019-04-13 21:44:03 +0800

    update: tag cloud style

--- example/pages/tags.vue
+++ example/pages/tags.vue
@@ -1,40 +1,66 @@
 <template>
-  <div>
-    <div v-for="(count, key) in tagsCount" :key="key" class="tag">
-      <saber-link :to="`/tags/${key}`">
-        <span :class="getWeight(count)">{{key}}</span>
-      </saber-link>
+    <div class="tag-cloud-tags">
+        <div v-for="(count, key) in tagsCount" :key="key" class="tag-cloud-tag">
+            <saber-link :to="`/tags/${key}`" :class="getSize(count)">
+                {{key}}
+            </saber-link>
+        </div>
     </div>
-  </div>
 </template>
-
 
 <script>
 export const attributes = {
-  layout: "default"
-};
+    title: "Tags",
+    layout: "default"
+}
 
 export default {
-  methods: {
-    getWeight(count) {
-      const length = this.$siteConfig.tags.length;
-      let weight = "regular";
-      if (count > length / 10) {
-        weight = "bold";
-      } else if (count < length / 30) {
-        weight = "light";
-      }
-      return weight;
+    methods: {
+        getSize(count) {
+            const length = this.$siteConfig.tags.length
+            let size = "medium"
+            if (count > length / 10) {
+                size = "large"
+            } else if (count < length / 30) {
+                size = "small"
+            }
+            return size
+        }
+    },
+
+    computed: {
+        tagsCount() {
+            const frequency = {}
+            this.$siteConfig.tags.forEach(tag => {
+                frequency[tag] = frequency[tag] ? frequency[tag] + 1 : 1
+            })
+            return frequency
+        }
     }
-  },
-  computed: {
-    tagsCount() {
-      const frequency = {};
-      this.$siteConfig.tags.forEach(tag => {
-        frequency[tag] = frequency[tag] ? frequency[tag] + 1 : 1;
-      });
-      return frequency;
-    }
-  }
-};
+}
 </script>
+
+<style scoped lang="scss">
+    .tag-cloud-tags {
+        font-size: 16px;
+        @media screen and (max-width: 760px) {
+            font-size: 12px;
+        }
+    }
+    .tag-cloud-tag {
+        display: inline-block;
+        margin: 0.625em;
+        a {
+            display: inline-block;
+            line-height: 1.6em;
+            transition: all .3s ease;
+            border-bottom: dashed rgba(158, 122, 122, .5) 1px;
+            &:hover {
+                border-bottom: dashed rgba(158, 122, 122, 1) 1px;
+            }
+        }
+        .small  { font-size: 1.2em; color: rgba(158, 122, 122, .60); }
+        .medium { font-size: 1.6em; color: rgba(158, 122, 122, .80); }
+        .large  { font-size: 2.0em; color: rgba(158, 122, 122, 1.0); }
+    }
+</style>
